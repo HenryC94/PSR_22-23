821744b0677a2469490c78d9f880ae5549f39c69
HenryC94 <33071636+HenryC94@users.noreply.github.com>
2022-10-13 13:13:04 +0100

    Update typing_test.py

--- typing_test.py
+++ typing_test.py
@@ -11,8 +11,7 @@ import time
 from pprint import pprint
 from collections import namedtuple
 
-def typetest(mv, utm):
-
+def typeTest(mv, utm):
     pool = string.ascii_lowercase # Gets all lowercase letters of the alphabet
     user = getlogin() # Gets the user's username
     now = datetime.datetime.now() # Gets current date
@@ -28,7 +27,6 @@ def typetest(mv, utm):
     test_start_date = test_start_date.strftime("%a %-d %b %-H:%M:%S %Y") # Stores test's start date and time
 
     if utm == True:
-
         print("Test running up to " + str(mv) + " seconds.")
         print('Press any key to start the test.')
 
@@ -36,42 +34,37 @@ def typetest(mv, utm):
         test_start_time = time.time() # Stores test's start time to count duration
 
         while time.time()-test_start_time < mv: # Runs loop while duration (mv) is not exceeded
+            start_input_time = time.time() # Stores time at input request
+            to_press = random.choice(pool) # selects a random letter to type
 
-            startinput_time = time.time() # Stores time at input request
-            topress = random.choice(pool) # selects a random letter to type
+            print('Type letter '+ Fore.LIGHTBLUE_EX + to_press + Fore.WHITE)
 
-            print('Type letter '+ Fore.LIGHTBLUE_EX + topress + Fore.WHITE)
+            key_press = readkey()# Reads pressed key
+            finishinput_time = time.time() - start_input_time # Stores time of input duration
 
-            keypress = readkey()# Reads pressed key
-            finishinput_time = time.time() - startinput_time # Stores time of input duration
-
-            if ord(keypress) == 32: # If spacebar is pressed, the program stops
-
+            if ord(key_press) == 32: # If spacebar is pressed, the program stops
                 break
 
-            elif ord(keypress) == ord(topress): # If the correct key is pressed, it will be shown in green
-
-                print("You typed letter " + Fore.GREEN + str(keypress) + Fore.WHITE)
+            elif ord(key_press) == ord(to_press): # If the correct key is pressed, it will be shown in green
+                print("You typed letter " + Fore.GREEN + str(key_press) + Fore.WHITE)
 
                 number_of_hits += 1
                 number_of_types += 1
 
-                typetuple = types(topress,keypress,finishinput_time)
-                inputs.append(typetuple)
+                type_tuple = types(to_press,key_press,finishinput_time)
+                inputs.append(type_tuple)
 
             else:
-
-                print("You typed letter " + Fore.RED + str(keypress) + Fore.WHITE) # If the wrong key is pressed, it will be shown in red
+                print("You typed letter " + Fore.RED + str(key_press) + Fore.WHITE) # If the wrong key is pressed, it will be shown in red
 
                 number_of_types += 1
 
-                typetuple = types(topress,keypress,finishinput_time)
-                inputs.append(typetuple)
+                type_tuple = types(to_press,key_press,finishinput_time)
+                inputs.append(type_tuple)
 
         stats(test_start_time, test_start_date, number_of_types, number_of_hits, inputs, utm, mv)
 
     else:
-
         print("Test running up to " + str(mv) + " inputs.")
         print('Press any key to start the test.')
 
@@ -79,37 +72,33 @@ def typetest(mv, utm):
         test_start_time = time.time()
 
         for i in range(mv):
+            start_input_time = time.time()
+            to_press = random.choice(pool)
 
-            startinput_time = time.time()
-            topress = random.choice(pool)
+            print('Type letter '+ Fore.LIGHTBLUE_EX + to_press + Fore.WHITE)
 
-            print('Type letter '+ Fore.LIGHTBLUE_EX + topress + Fore.WHITE)
+            key_press = readkey()
+            finishinput_time = time.time() - start_input_time
 
-            keypress = readkey()
-            finishinput_time = time.time() - startinput_time
-
-            if ord(keypress) == 32:
-
+            if ord(key_press) == 32:
                 break
 
-            elif ord(keypress) == ord(topress):
-
-                print("You typed letter " + Fore.GREEN + str(keypress) + Fore.WHITE)
+            elif ord(key_press) == ord(to_press):
+                print("You typed letter " + Fore.GREEN + str(key_press) + Fore.WHITE)
 
                 number_of_hits += 1
                 number_of_types += 1
 
-                typetuple = types(topress,keypress,finishinput_time)
-                inputs.append(typetuple)
+                type_tuple = types(to_press,key_press,finishinput_time)
+                inputs.append(type_tuple)
 
             else:
-
-                print("You typed letter " + Fore.RED + str(keypress) + Fore.WHITE)
+                print("You typed letter " + Fore.RED + str(key_press) + Fore.WHITE)
 
                 number_of_types += 1
 
-                typetuple = types(topress,keypress,finishinput_time)
-                inputs.append(typetuple)
+                type_tuple = types(to_press,key_press,finishinput_time)
+                inputs.append(type_tuple)
 
         stats(test_start_time, test_start_date, number_of_types, number_of_hits, inputs, utm, mv)
 
@@ -120,58 +109,40 @@ def stats(test_start_time, test_start_date, number_of_types, number_of_hits, inp
         print(Fore.LIGHTBLUE_EX + "Test finished!" + Fore.WHITE)
 
         if utm == True:
-
             print("Curernt test duration (" + str(test_duration) + " ) exceed maximum of " + str(mv) + ".")
 
         test_end_date = datetime.datetime.now()
         test_end_date = test_end_date.strftime("%a %-d %b %-H:%M:%S %Y")
 
         if number_of_types != 0 :
-
             average_type = test_duration / number_of_types
-
             accuracy = number_of_hits / number_of_types
 
         else:
-
             average_type = 0
-
             accuracy = 0
             
         if number_of_hits != 0 :
-
             average_hit = test_duration / number_of_hits
 
         else:
-
             average_hit = 0
 
         if number_of_types - number_of_hits > 0 :
-
             average_miss = test_duration / (number_of_types - number_of_hits)
 
         else:
-
             average_miss = 0
 
         resultado = {"Accuracy" : accuracy,
-
                     "Number of hits" : number_of_hits,
-
                     "Number of types" : number_of_types,
-
                     "Test duration" : test_duration,
-
                     "Test start" : test_start_date,
-
                     "Test end" : test_end_date,
-
                     "Type average duration" : average_type,
-
                     "Type hit average duration" : average_hit,
-
                     "Type miss average duration" : average_miss,
-
                     "Types" : inputs}
 
         pprint(resultado)
@@ -189,7 +160,7 @@ def main():
 
     if args.mv != None:
 
-        typetest(args.mv,args.utm) #calls the test function
+        typeTest(args.mv,args.utm) #calls the test function
 
     else:
 
